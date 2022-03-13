fd6551c5777261bc70a87667129b44ed343c6042
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2022-03-13 23:13:17 +0700

    More type functions

---
+++ src/MoreOnFunctions.md
@@ -0,0 +1,14 @@
+### Function Type Expressions
+### Generic Functions
+### Inference
+### Constraints
+
+- `extends`
+
+### void, object, unknown, never, Function
+
+- `void`: represents the return value of functions which don’t return a value
+- `object`: refers to any value that isn’t a primitive, different from the empty object type `{ }`
+- `unknown`: type represents any value,  similar to the `any` type, but it’s not legal to do anything with an `unknown` value
+- `never`: type represents values which are `never` observed
+- `Function`: describes properties like `bind`, `call`, `apply`, and others present on all function values in JavaScript
--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,24 @@ let obj = {
     [sym]: 'value',
 };
 console.log(obj[sym]);
+
+interface DescribableFunction {
+    description: string;
+    (someArg: number): boolean;
+}
+
+function doSomething(fn: DescribableFunction) {
+    console.log(fn.description + ' returned ' + fn(6));
+}
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+    if (d !== undefined && y !== undefined) {
+        return new Date(y, mOrTimestamp, d);
+    } else {
+        return new Date(mOrTimestamp);
+    }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
